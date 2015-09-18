4d1839f2b1956f5d4292118fcfb1a28b204a13c2
Tannor Ziehm <ziehmtj@s.dcsdk12.org>
2015-09-18 10:23:55 -0600

    I made a quizy thing

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,5 +1,88 @@
-module MethodsQuiz
+module MethodsQuiz2
 	
-	
+	def without_doubles(num1, num2, no_doubles)
+		if ((num1 == num2) && no_doubles)
+			if ((num1 == 6) || (num2 == 6))
+				num1 + 1
+			else
+				num1 + num2 + 1
+			end
+		else
+			num1 + num2
+		end
+	end
+
+	def max_maybe(num1, num2)
+		if(num1 == num2)
+			0
+		elsif(num1 % 5 == num2 % 5)
+			larger?(num1, num2)
+		else
+			smaller?(num1, num2)
+		end
+	end
+
+	def squirrels_play?(temp, summer)
+		if((summer && summer_temp(temp)) || play_temp(temp))
+				return "Squirrels shall play"
+		else
+			return "Squirrels shan't play"
+		end
+	end
+
+	def red_ticket(a,b,c)
+		if(all_two(a,b,c))
+			return 10
+		elsif((a == b) && (b == c))
+			return 5
+		elsif(different?(a,b,c))
+			return 1
+		else
+			return 0
+		end
+	end
+
+
+
+
+		
+
+
+	private
+
+		def different?(a,b,c)
+			a != b && a != c ? true : false
+		end
+
+		def all_two(a,b,c)
+			((a && b && c) == 2)
+		end
+
+		
+
+		def summer_temp(temp)
+			((temp >= 60) && (temp <= 100))
+		end
+
+		def play_temp(temp)
+			(temp >= 60) && (temp <= 90)
+		end
+
+		def smaller?(num1, num2)
+			if(num1 > num2)
+				num1
+			else
+				num2
+			end
+		end
+
+		def larger?(num1, num2)
+			if(num1 < num2)
+				num1
+			else
+				num2
+			end
+		end
+
 
 end
--- methods_quiz2_test.rb
+++ methods_quiz2_test.rb
@@ -11,6 +11,35 @@ class MethodsQuizTest < MiniTest::Test
    	end.new
 	end
 
-	# TODO - write tests here
+	def test_without_doubles
+		assert_equal 5, @m.without_doubles(3, 2, false)
+		assert_equal 6, @m.without_doubles(3, 3, false)
+		assert_equal 7, @m.without_doubles(3, 3, true)
+		assert_equal 5, @m.without_doubles(3, 2, true)
+		assert_equal 7, @m.without_doubles(6, 6, true)
+	end
+
+	def test_max_maybe
+		assert_equal 3, @m.max_maybe(3,2)
+		assert_equal 1, @m.max_maybe(6,1)
+		assert_equal 0, @m.max_maybe(3,3)
+	end
+
+	def test_squirrels_play?
+		assert_equal "Squirrels shall play", @m.squirrels_play?(60, true)
+		assert_equal "Squirrels shall play", @m.squirrels_play?(60, false)
+		assert_equal "Squirrels shan't play", @m.squirrels_play?(50, true)
+		assert_equal "Squirrels shan't play", @m.squirrels_play?(50, false)
+		assert_equal "Squirrels shan't play", @m.squirrels_play?(95, false)
+		assert_equal "Squirrels shall play", @m.squirrels_play?(95, true)
+	end
+
+	def test_red_ticket
+		assert_equal 10, @m.red_ticket(2,2,2)
+		assert_equal 5, @m.red_ticket(1,1,1)
+		assert_equal 1, @m.red_ticket(2,1,1)
+		assert_equal 0, @m.red_ticket(2,2,1)
+		assert_equal 1, @m.red_ticket(2,3,4)
+	end
 
 end
